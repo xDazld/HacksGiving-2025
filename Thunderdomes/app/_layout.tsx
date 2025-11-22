9ed15979b43ef9dbc483ad216bb51d00fcd29b04
Alexander Lopez <lopezaj@msoe.edu>
2025-11-21 23:19:02 -0600

    Remove modal screen and its navigation route
    Deleted the modal screen component and removed its corresponding navigation route from the stack in _layout.tsx, as it is no longer needed.

--- Thunderdomes/app/_layout.tsx
+++ Thunderdomes/app/_layout.tsx
@@ -37,7 +37,6 @@ function RootLayoutNav() {
       <Stack>
         <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
